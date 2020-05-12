9b43017aed95f92a3dbacf74202607e33116aa51
SLJacobs <sljacobs910@gmail.com>
2020-05-12 11:22:14 -0400

    excercise 5 & 6

--- com.conferencedemo/src/main/java/com/conferencedemo/Application.java
+++ com.conferencedemo/src/main/java/com/conferencedemo/Application.java
@@ -3,8 +3,10 @@ package com.conferencedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	@Value("${dbname}")
